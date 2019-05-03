d61681f422c6204038ce2e60bba028cc166224f3
Zahid <zahidhossain0033@gmail.com>
2019-05-03 17:59:28 +0600

    notice files delete

--- admin/includes/delete_notice.php
+++ admin/includes/delete_notice.php
@@ -2,6 +2,18 @@
  require "../../db/config.php";
     $id = $_GET['id'];
 
+    $statement = "select * from notice where id = '$id' ";
+	$result = mysqli_query($conn,$statement);
+	$count =  mysqli_num_rows($result);
+	if ($count == 1) {
+		while ($row = mysqli_fetch_assoc($result)) {
+			$file = $row['image'];
+			if ($file != "") {
+				unlink("../../uploads/".$file);
+			}
+		}
+	}
+
     $result = mysqli_query($conn,"delete from notice WHERE id='$id' ");
 if(!$result){
     die("query failed".mysqli_error($conn));
